20cf7aad48340f482a87a826dde8e3a0751ccc55
deniz0zn <119049752+deniz0zn@users.noreply.github.com>
2025-01-03 12:32:53 +0100

    constants moved to config.py and evaluation class is running in the process.py

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 
 dataset_path = 'Dataset/csv/Hospital Billing - Event Log.csv'
-initial_months = 8
+initial_months = 12
 frequency = 'weekly'  # 'daily', 'weekly', or 'monthly'
 
 filename = os.path.splitext(os.path.basename(dataset_path))[0]
 output_dir = f'Dataset/Hospital Billing Delta Logs/{filename}_{frequency}_({initial_months})'
+
+delta_dir_path = "Dataset/Hospital Billing Delta Logs/"
+cases_output_path = f"Dataset/Hospital Billing Delta Logs/cases_output/cases_output_{filename}_{frequency}_({initial_months}).csv"
+delta_output_path = f"Dataset/Hospital Billing Delta Logs/Delta Stats/delta_stats_{filename}_{frequency}_({initial_months}).csv"
+
+max_days = 190
+sample_size = 500
+__RANDOM_SEED__ = 420
+
+Evaluate = True
+Plot = True
--- process.py
+++ process.py
@@ -1,9 +1,13 @@
 import os
 import pandas as pd
-from delta_log_formation import EventLogSplitter  # Updated import
+from tqdm import tqdm
+from delta_log_formation import EventLogSplitter
+from evaluation import CompletenessEvaluator
 from case import Case
 from delta import Delta
-from config import dataset_path, initial_months, frequency, filename, output_dir
+from config import (dataset_path, initial_months, frequency, filename,
+                    output_dir, sample_size, cases_output_path, delta_output_path,
+                    Evaluate, Plot)
 
 
 class ProcessManager:
@@ -34,27 +38,27 @@ class ProcessManager:
         event_log = pd.read_csv(path)
         delta = Delta(delta_name)
 
-        for _, event in event_log.iterrows():
-            case_id = event.get("case")
+        current_T = event_log["completeTime"].iloc[-1]
+        if delta_name != "initial_log":
+            for case_id, case_obj in tqdm(self.cases.items(), desc=f"Processing cases for {delta_name}"):
+                case_obj.increase_delta_counter()
+                case_obj.update_time(current_T)
+                delta.process_case_status(case_obj)
 
+        for _, event in tqdm(event_log.iterrows(), total=len(event_log), desc=f"Processing events for {delta_name}"):
+            case_id = event.get("case")
             # Initialize or update cases
             if self.cases.get(case_id) is None:
                 self.cases[case_id] = Case(event, delta_name, delta)
             else:
                 self.cases[case_id].update(event, delta)
 
-        # Update statuses and time tracking for all cases
-        current_T = event_log["completeTime"].iloc[-1]
-        for case_id, case_obj in self.cases.items():
-            case_obj.update_time(current_T, delta_name)
-            delta.process_case_status(case_obj)
-
         self.delta_stats_list.append(delta.generate_report())
 
     def save_delta_statistics(self):
         """Save delta statistics to a CSV file."""
         delta_df = pd.DataFrame(self.delta_stats_list)
-        delta_output_path = f"Dataset/Hospital Billing Delta Logs/Delta Stats/delta_stats_{filename}_{frequency}_({initial_months}).csv"
+
         delta_df.to_csv(delta_output_path, index=False)
         print(f"Delta Statistics saved to: {delta_output_path}")
 
@@ -74,9 +78,21 @@ class ProcessManager:
         print(f"Number of Complete Cases (Without Cancelled cases): {len(completed_cases)}")
         print(f"Ratio of Completeness out of not cancelled cases: {((len(completed_cases) / len(not_cancelled)) * 100):.2f}%")
 
-        output_path = f"Dataset/Hospital Billing Delta Logs/cases_output/cases_output_{filename}_{frequency}_({initial_months}).csv"
+        output_path = cases_output_path
         case_df.to_csv(output_path, index=True)
         print(f"Final Results saved to: {output_path}")
+
+    def perform_evaluation(self, event_log_path, case_output_path):
+        evaluator = CompletenessEvaluator(event_log_path, case_output_path)
+
+        sampled_events, sampled_case_ids = evaluator.sample_cases(sample_size)
+        manually_labeled_cases = evaluator.manually_label_cases(sampled_events)
+
+        metrics = evaluator.evaluate_predictions(sampled_case_ids, manually_labeled_cases)
+        report = evaluator.generate_report(metrics)
+        return report
+
+
 
     def run(self):
         """Run the full processing pipeline."""
@@ -99,11 +115,13 @@ class ProcessManager:
 
         # Process delta logs
         for file, delta_name in delta_logs:
-            print(f"[PROCESS MANAGER] Processing delta log file: {file}")
+            print(f"[PROCESS MANAGER] Processing delta log file: {delta_name}")
             self.process_logs(file, delta_name)
 
         # Save results
         self.save_delta_statistics()
         self.save_case_statistics()
 
+        evaluation_report = self.perform_evaluation(dataset_path ,cases_output_path)
+        print(evaluation_report)
 
