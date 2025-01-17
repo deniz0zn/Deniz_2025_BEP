b62e523a73d74ba61e629903a721fe57e6de8b68
deniz0zn <119049752+deniz0zn@users.noreply.github.com>
2025-01-17 06:55:18 +0100

    New scenario for completeness is added (Rejected Unbillable cases)

--- case.py
+++ case.py
@@ -14,10 +14,13 @@ class Case:
     def initialize_case_attributes(self, event, delta_name: str, delta: Delta):
         """Initialize the primary attributes of the Case."""
         self.critical_events = {"BILLED", "FIN", "RELEASE", "CODE OK"}
+        self.rejected_events = {"STORNO", "REJECT"}
 
         self.case_id = event.get("case")
-        self.completeness_status = "ONGOING"
+        self.final_status = "ONGOING"
         self.status_transitions = []
+        self.transition_count = 0
+        self.first_transition_to = None
         self.last_state = event.get("state")
         self.last_event = event.get("event")
         self.unique_events = {self.last_event}
@@ -30,14 +33,12 @@ class Case:
         self.complete = False
         self.incomplete = None
         self.isBilled = self.last_state == "Billed"
+        self.isUnbillable = self.last_state == "Unbillable"
         self.have_crit_events = False
-        self.issues = [f"Missing critical events: {self.missing_events}",
-                       "Case is not billed.",
-                       "Case is not finished.",
-                       ]
+        self.issues = "No updates received"
 
         self.short = True
-        self.t_since_last_event = timedelta(0)
+        self.t_since_last_event = 0
         self.event_gaps = [self.t_since_last_event]
         self.avg_wait_time = None
 
@@ -46,10 +47,11 @@ class Case:
 
         self.first_delta = delta_name
         self.last_delta_update = delta_name
-        self.delta_counts = [0]
+        self.delta_counts_array = [0]
 
         self.missing_attributes = {}
         self.n_events_w_missing_attr = 0
+        self.status_trace = ["ONGOING"]
 
     def initialize_timestamps(self, event):
         """Initialize the timestamp attributes."""
@@ -62,7 +64,6 @@ class Case:
 
     def register_new_case(self, delta: Delta, event):
         """Register the case as a new case in the Delta object."""
-        delta.new_cases += 1
         delta.initialised_cases.add(self.case_id)
         delta.process_event(event)
 
@@ -72,7 +73,7 @@ class Case:
         self.update_event_attributes(event)
         self.update_case_status(event, delta)
         self.update_time_gap(event)
-        self.run_function_and_update_status(delta.delta_file_name,self.completeness_status ,self.check_completeness())      #self.check_completeness()
+        self.run_function_and_update_status(delta.delta_file_name, self.final_status, self.check_completeness())
         self.append_delta(delta, delta_counts)
         delta.process_event(event)
     def update_event_attributes(self, event):
@@ -86,16 +87,19 @@ class Case:
     def update_case_status(self, event, delta: Delta):
         """Update the case status attributes."""
         self.cancelled = self.run_function_and_update_status(delta.delta_file_name,
-                                                             self.completeness_status,
+                                                             self.final_status,
                                                              self.check_cancelled(event))
         self.isBilled = self.last_state == "Billed"
+        self.isUnbillable = self.last_state == "Unbillable"
+
         self.have_crit_events = self.crit_event_check()
 
         self.short = self.length < 5
         self.sleep = False
         self.ongoing = self.run_function_and_update_status(delta.delta_file_name,
-                                                           self.completeness_status,
+                                                           self.final_status,
                                                            self.check_ongoing())
+
 
         if self.case_id not in delta.initialised_cases:
             delta.ongoing_cases_count.add(self.case_id)
@@ -108,13 +112,13 @@ class Case:
                         if isinstance(complete_time, str) else complete_time)
 
         self.t_since_last_event = current_time - self.last_event_time
-        self.event_gaps.append(self.t_since_last_event)
-        self.avg_wait_time = sum(self.event_gaps, timedelta(0)) / len(self.event_gaps)
+        self.event_gaps.append(self.t_since_last_event.total_seconds())
+        self.avg_wait_time = sum(self.event_gaps) / len(self.event_gaps)
         self.last_event_time = current_time
 
 
     def append_delta(self,delta: Delta, delta_counts: pd.DataFrame):
-        self.delta_counts.append(delta_counts.loc[self.case_id, "count"])
+        self.delta_counts_array.append(delta_counts.loc[self.case_id, "count"])
         self.last_delta_update = delta.delta_file_name
 
 
@@ -127,14 +131,22 @@ class Case:
 
     def crit_event_check(self):
         """Check if all critical events are present."""
-        not_missing = self.critical_events.issubset(self.unique_events)
-        self.missing_events = self.critical_events - self.unique_events
+        if not self.isUnbillable:
+            not_missing = self.critical_events.issubset(self.unique_events)
+            self.missing_events = self.critical_events - self.unique_events
+            return not_missing
 
-        return not_missing
+        else:
+            new_essential_events = self.rejected_events.union(self.critical_events)
+            not_missing = (new_essential_events.issubset(self.unique_events))
+            self.missing_events = new_essential_events - self.unique_events
+            return not_missing
+
+
 
     def run_function_and_update_status(self, delta_name: str, previous_status ,function: callable ):
         value = function
-        new_status = self.completeness_status
+        new_status = self.final_status
         transition = {
             "previous": previous_status,
             "new": new_status,
@@ -143,40 +155,44 @@ class Case:
 
         if previous_status!= new_status:
             self.status_transitions.append(transition)
+            self.status_trace.append(new_status)
+            self.transition_count += 1
+
+        if (self.first_transition_to == None) and (self.transition_count > 0):
+            self.first_transition_to = self.final_status
 
         if value != None:
             return value
     def check_completeness(self, returning = False):
         """Evaluate whether the case is complete."""
-        self.issues.clear()
+        self.issues = ""
         completeness = True
 
         if not self.cancelled:
-            if not self.have_crit_events:
-                completeness = False
-                self.issues.append(f"Missing critical events: {self.missing_events}")
-
-            # if not self.isBilled:
-            #     completeness = False
-            #     self.issues.append("Case is not billed.")
-
             if self.ongoing:
                 completeness = False
-                self.issues.append("Case is not finalised. (Not Billed or Cancelled)")
-        # else:
-        #     completeness = None
+                self.issues = "Trace is not finalised."
+
+            # elif (self.isUnbillable) and not (self.have_crit_events):
+            #     completeness = False
+            #     self.issues = f"Missing events: {self.missing_events}"
+
+            elif not self.have_crit_events:
+                completeness = False
+                self.issues = f"Missing events: {self.missing_events}"
+
 
         self.complete = completeness
         if completeness:
-            self.completeness_status = "COMPLETE"
+            self.final_status = "COMPLETE"
             self.ongoing = False
             self.incomplete = False
         else:
             self.incomplete = None
 
-        if self.cancelled:
-            self.completeness_status = "CANCELLED"
-            self.ongoing = False
+        # if self.cancelled:
+        #     self.final_status = "CANCELLED"
+        #     self.ongoing = False
 
 
         return completeness if returning else None
@@ -185,18 +201,18 @@ class Case:
         iscancelled = event.get('isCancelled', False)
         self.cancelled = iscancelled
         if iscancelled:
-            self.completeness_status = "CANCELLED"
+            self.final_status = "COMPLETE"
         return iscancelled
 
     def check_ongoing(self):
-        isOngoing = not (self.cancelled or self.isBilled)
+        isOngoing = not (self.cancelled or self.isBilled or self.isUnbillable)
         if isOngoing:
-            self.completeness_status = "ONGOING"
+            self.final_status = "ONGOING"
         return isOngoing
 
     def update_sleep(self):
         self.sleep = True
         self.ongoing = False
         self.incomplete = True
-        self.completeness_status = "INCOMPLETE"
+        self.final_status = "INCOMPLETE"
 
