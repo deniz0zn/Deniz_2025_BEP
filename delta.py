7e70c7433ed4447953b55fe627bf92ce6d5d5f2b
deniz0zn <119049752+deniz0zn@users.noreply.github.com>
2025-01-26 22:34:00 +0100

    Bugs fixed in delta.py

--- delta.py
+++ delta.py
@@ -4,15 +4,15 @@ class Delta:
 
     def __init__(self, delta_file_name):
         # Initialize attributes to track statistics for the delta file
-        self.delta_file_name = delta_file_name
+        self.delta_file_name = delta_file_name[:8]
         self.event_counter = Counter()
-        self.not_finished = 0
-        self.complete_cases = 0
-        self.incomplete_cases = 0
-        self.cancelled = 0
+        self.not_finished = set()
+        self.complete_cases = set()
+        self.incomplete_cases = set()
+        self.cancelled = set()
         self.total_event = sum(dict(self.event_counter).values())
         self.initialised_cases = set()
-        self.ongoing_cases_count= set()
+        self.ongoing_cases_count = set()
 
 
 
@@ -38,24 +38,22 @@ class Delta:
         COMPLETE = (case.final_status == "COMPLETE") and not (CANCELLED)
         ONGOING = (case.final_status == "ONGOING")
 
-
         case_info = self.case_info
-
         if CANCELLED:
             case_info["cancelled"].add(case.case_id)
-            self.cancelled = len(case_info["cancelled"])
+            self.cancelled = case_info["cancelled"]
 
         if COMPLETE:
             case_info["complete"].add(case.case_id)
-            self.complete_cases = len(case_info["complete"])
+            self.complete_cases = case_info["complete"]
 
         if ONGOING:
             case_info["not_finished"].add(case.case_id)
-            self.not_finished = len(case_info["not_finished"])
+            self.not_finished = case_info["not_finished"]
 
-        if not(CANCELLED) and not (ONGOING) and not(COMPLETE):
-            case_info["incomplete"].add(case.case_id)
-            self.incomplete_cases = len(case_info["incomplete"])
+        # if not(CANCELLED) and not (ONGOING) and not(COMPLETE):
+        #     case_info["incomplete"].add(case.case_id)
+        #     self.incomplete_cases = len(case_info["incomplete"])
 
     def generate_report(self):
         """
@@ -67,14 +65,19 @@ class Delta:
             "delta_file_name": self.delta_file_name,
             "event_counts": dict(self.event_counter),
             "total_events": sum(dict(self.event_counter).values()),
-            "not_finished": self.not_finished,
-            "cancelled_cases": self.cancelled,
-            "complete_cases": self.complete_cases,
-            "incomplete_cases": self.incomplete_cases,
-            "initialised_cases_count": len(self.initialised_cases),
-            "updated_cases_count": len(self.ongoing_cases_count),
-            "cases_processed" : len(self.initialised_cases) + len(self.ongoing_cases_count),
+            "ongoing_count": len(self.not_finished),
+            "cancelled_count": len(self.cancelled),
+            "complete_count": len(self.complete_cases),
+            "incomplete_count": len(self.incomplete_cases),
+            "initialised_count": len(self.initialised_cases),
+            "updated_count": len(self.ongoing_cases_count),
+            "cases_processed": len(self.initialised_cases) + len(self.ongoing_cases_count),
             "initialised_cases": self.initialised_cases,
             "updated_cases": self.ongoing_cases_count,
+            "complete_cases": self.complete_cases,
+            "incomplete_cases": self.incomplete_cases,
+            "cancelled_cases": self.cancelled,
+            "ongoing_cases": self.not_finished,
+
          }
 
