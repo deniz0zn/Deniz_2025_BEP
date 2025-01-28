03626969f40d6e76bef6215dcdfc0e4c563bbc8a
deniz0zn <119049752+deniz0zn@users.noreply.github.com>
2025-01-28 02:54:07 +0100

    evaluation output path added into the config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
 
-dataset_path = 'Dataset/csv/Hospital Billing - Event Log.csv'
+event_log_path = 'Dataset/csv/Hospital Billing - Event Log.csv'
 initial_months = 1
 frequency = 'weekly'  # 'daily', 'weekly', or 'monthly'
 
-filename = os.path.splitext(os.path.basename(dataset_path))[0]
+filename = os.path.splitext(os.path.basename(event_log_path))[0]
 delta_log_dir = f'Dataset/Hospital Billing Delta Logs/{filename}_{frequency}_({initial_months})'
 
 delta_dir_path = "Dataset/Hospital Billing Delta Logs/"
 cases_output_path = f"Dataset/Hospital Billing Delta Logs/cases_output/cases_output_{frequency}_({initial_months}).csv"
 delta_output_path = f"Dataset/Hospital Billing Delta Logs/Delta Stats/delta_stats_{frequency}_({initial_months}).csv"
+evaluation_output_path = f"Dataset/Hospital Billing Delta Logs/evaluation/eval_{frequency}_({initial_months}).csv"
 
 max_days = 190
 
@@ -17,8 +18,6 @@ sample_size = 100
 __RANDOM_SEED__ = 31
 
 test_eval = True  # DEFAULT: False
-
-evet_counter = dict()
 
 attributes_to_check = ["case", "event", "startTime", "completeTime",
                        "isCancelled", "blocked", "isClosed", "state"
